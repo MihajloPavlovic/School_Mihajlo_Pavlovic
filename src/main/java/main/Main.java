a91e7b8e388847fe005016d91dc6ede472a9d1f0
Mihajlo <mihajlo0709@gmail.com>
2021-03-22 13:02:33 +0100

    finished task

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,10 +1,25 @@
 package main;
 
+import student.Student;
+
 public class Main {
 
     
     public static void main(String[] args) {
       
+        Student student1 = new Student();
+        
+        student1.setFirstName("Petar");
+        student1.setLastName("Petrovic");
+        student1.setYearOfBirth(1988);
+        student1.getCourse().setName("QA");
+        student1.getCourse().setNumberOfClasses(40);
+        student1.getCourse().setCodeName("");
+        student1.getComputer().setOperatingSystem("Linux");
+        student1.getComputer().setProcessTact(2.4);
+        student1.getComputer().setMemory(16.0);
+        student1.getComputer().setHardDrive(500);
+        student1.info();
     }
     
 }
